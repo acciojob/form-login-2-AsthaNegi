986c4ce11c356eb761b756c944a7a0ed209beb12
Astha Negi <75171246+AsthaNegi@users.noreply.github.com>
2026-03-24 16:16:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const form=document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+   e.preventDefault();
+   
+  const firstName=document.getElementById("firstName").value; 
+let lastName = document.getElementById("lastName").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+
+  let message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
+
+  alert(message);
+
+});
